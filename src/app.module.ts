e2df23c3a02292cea4491f9030806588c17e9db6
smillanpardo <santiago.millan@nivelics.co>
2024-02-04 04:57:12 -0500

    prueba para despliegue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { AllowAnyIPMiddleware } from './middleware/auth.middleware';
 
 @Module({
   imports: [HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AllowAnyIPMiddleware).forRoutes('*');
+  }
+}
